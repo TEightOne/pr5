8df2d25a634c18e466d114afea2919a41b3692f9
TEightOne <90195829+TEightOne@users.noreply.github.com>
2021-12-13 13:47:42 +0300

    Update pr5.java

--- pr5/src/pr5.java
+++ pr5/src/pr5.java
@@ -1,9 +1,5 @@
 public class pr5 {
-    // ex 6
-    /*
-    Дано натуральное число n>1. Проверьте, является ли оно простым.
-    Программа должна вывести слово YES, если число простое и NO, если число составное.
-     */
+
     private static void isPrime(int number) {isPrime(number, 2);}
     private static void isPrime(int number, int div){
         if (number == 2) {
@@ -21,14 +17,7 @@ public class pr5 {
         isPrime(number, div+1);
     }
 
-    // ex7
 
-    /*
-Дано натуральное число n>1.
-Выведите все простые множители этого числа в порядке не убывания с учетом кратности.
-     */
-
-    // methods without souts
     private static boolean isNumPrime(int number){
         return isNumPrime(number, 2);
     }
@@ -60,11 +49,6 @@ public class pr5 {
         }else factorization(number, div+1);
     }
 
-    //ex8
-    /*
-    Дано слово, состоящее только из строчных латинских букв.
-    Проверьте, является ли это слово палиндромом. Выведите YES или NO.
-     */
     private static void isPalindrome(String s){
         if(s.length() == 1){
             System.out.println("YES"); return;
@@ -78,22 +62,12 @@ public class pr5 {
         isPalindrome(s.substring(1, s.length()-1));
     }
 
-    // ex9
-    /*
-    Даны числа a и b. Определите, сколько существует последовательностей из a нулей и b единиц, в которых никакие два нуля не стоят рядом.
-     */
     private static int count(int a, int b){
         if(a==0) return 1;
         if(a==1) return b+1;
         if(a>1 && b==0) return 0;
         return count(a-1, b-1) + count(a, b-1);
     }
-
-//ex10
-    /*
-    Дано число n, десятичная запись которого не содержит нулей.
-    Получите число, записанное теми же цифрами, но в противоположном порядке.
-     */
 
     private static int revers(int num, int rev){
         if(num==0)return rev;
@@ -102,12 +76,12 @@ public class pr5 {
 
 
     public static void main(String[] args) {
-//ex6
+
         System.out.println("Primes?");
         isPrime(2);
-        isPrime(18756);
-        isPrime(3571); // prime
-//ex7
+        isPrime(13);
+        isPrime(4); // prime
+
         System.out.println("\nFactorization");
         factorization(525);
         System.out.println();
@@ -116,21 +90,21 @@ public class pr5 {
         factorization(360);
         System.out.println();
         factorization(2);
-//ex8
-        System.out.println("\nPalindrome:");
-        isPalindrome("hellolleh");
-        isPalindrome("heloeh");
-        isPalindrome("h");
 
-//ex9
-        System.out.println("\nZeros");
-        System.out.println(count(0,0));
+        System.out.println("\nPalindrome:");
+        isPalindrome("kazak");
+        isPalindrome("anton");
+        isPalindrome("n");
+
+
+        System.out.println("\n0 and 1");
+        System.out.println(count(4,5));
         System.out.println(count(1,3));
-        System.out.println(count(14,15));
-//ex10
+        System.out.println(count(11,17));
+
         System.out.println("\nReverse");
-        System.out.println(revers(123,0));
-        System.out.println(revers(1, 0));
-        System.out.println(revers(9968123, 0));
+        System.out.println(revers(12345,0));
+        System.out.println(revers(1337, 0));
+        System.out.println(revers(865, 0));
     }
 }
